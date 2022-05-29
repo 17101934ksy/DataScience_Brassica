d0b04ee6b2535c12f1611302d7689e8a21387a22
Koseyun <88478829+17101934ksy@users.noreply.github.com>
2022-05-30 01:20:59 +0900

    Update loader.py

--- loader.py
+++ loader.py
@@ -2,7 +2,7 @@ import os
 import sys
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
-from moduler import *
+from modular import *
 
 class CustomDataset(Dataset):
     def __init__(self, img_list, meta_list, label_list, train_mode=True, transforms=None):
