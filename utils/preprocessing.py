9037d683c6b3a2bd6fe0473f47e7a7af780ebee8
Koseyun <88478829+17101934ksy@users.noreply.github.com>
2022-05-30 01:22:06 +0900

    Update preprocessing.py

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -2,7 +2,7 @@ import os
 import sys
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
-from moduler import *
+from modular import *
 
 def get_train_data(data_dir):
     img_path_list = []
